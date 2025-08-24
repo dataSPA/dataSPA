2b5eb9d81e2637c2a27de95bfc5953eebe412d1c
Felix Ingram <f.ingram@gmail.com>
2025-08-24 22:37:41 +0100

    Add plugins to the bundles

--- library/src/engine/consts.ts
+++ library/src/engine/consts.ts
@@ -6,3 +6,5 @@ export const DATASTAR_PROP_CHANGE_EVENT = 'datastar-prop-change'
 export const DATASTAR_READY_EVENT = 'datastar-ready'
 export const DATASTAR_SCOPE_CHILDREN_EVENT = 'datastar-scope-children'
 export const DATASTAR_SIGNAL_PATCH_EVENT = 'datastar-signal-patch'
+
+export const DATASTAR = "datastar"
