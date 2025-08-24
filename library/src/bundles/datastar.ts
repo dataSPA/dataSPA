d297d9efe9620357230328c5462f63fc9b3545ad
Felix Ingram <f.ingram@gmail.com>
2025-08-24 22:37:41 +0100

    Add plugins to the bundles

--- library/src/bundles/datastar.ts
+++ library/src/bundles/datastar.ts
@@ -1,31 +1,36 @@
-import { apply, load, setAlias } from '../engine'
-import { Peek } from '../plugins/actions/peek'
-import { SetAll } from '../plugins/actions/setAll'
-import { ToggleAll } from '../plugins/actions/toggleAll'
-import { Attr } from '../plugins/attributes/attr'
-import { Bind } from '../plugins/attributes/bind'
-import { Class } from '../plugins/attributes/class'
-import { Computed } from '../plugins/attributes/computed'
-import { Effect } from '../plugins/attributes/effect'
-import { Indicator } from '../plugins/attributes/indicator'
-import { JsonSignals } from '../plugins/attributes/jsonSignals'
-import { On } from '../plugins/attributes/on'
-import { OnIntersect } from '../plugins/attributes/onIntersect'
-import { OnInterval } from '../plugins/attributes/onInterval'
-import { OnLoad } from '../plugins/attributes/onLoad'
+import { apply, load, setAlias } from "../engine";
+import { Peek } from "../plugins/actions/peek";
+import { SetAll } from "../plugins/actions/setAll";
+import { ToggleAll } from "../plugins/actions/toggleAll";
+import { Attr } from "../plugins/attributes/attr";
+import { Bind } from "../plugins/attributes/bind";
+import { Class } from "../plugins/attributes/class";
+import { Computed } from "../plugins/attributes/computed";
+import { Effect } from "../plugins/attributes/effect";
+import { Indicator } from "../plugins/attributes/indicator";
+import { JsonSignals } from "../plugins/attributes/jsonSignals";
+import { On } from "../plugins/attributes/on";
+import { OnIntersect } from "../plugins/attributes/onIntersect";
+import { OnInterval } from "../plugins/attributes/onInterval";
+import { OnLoad } from "../plugins/attributes/onLoad";
 import { OnSignalPatch } from "../plugins/attributes/onSignalPatch";
-import { Ref } from '../plugins/attributes/ref'
-import { Show } from '../plugins/attributes/show'
-import { Signals } from '../plugins/attributes/signals'
-import { Style } from '../plugins/attributes/style'
-import { Text } from '../plugins/attributes/text'
-import { DELETE } from '../plugins/backend/actions/delete'
-import { GET } from '../plugins/backend/actions/get'
-import { PATCH } from '../plugins/backend/actions/patch'
-import { POST } from '../plugins/backend/actions/post'
-import { PUT } from '../plugins/backend/actions/put'
-import { PatchElements } from '../plugins/backend/watchers/patchElements'
-import { PatchSignals } from '../plugins/backend/watchers/patchSignals'
+import { Ref } from "../plugins/attributes/ref";
+import { Show } from "../plugins/attributes/show";
+import { Signals } from "../plugins/attributes/signals";
+import { Style } from "../plugins/attributes/style";
+import { Text } from "../plugins/attributes/text";
+import { DELETE } from "../plugins/backend/actions/delete";
+import { GET } from "../plugins/backend/actions/get";
+import { PATCH } from "../plugins/backend/actions/patch";
+import { POST } from "../plugins/backend/actions/post";
+import { PUT } from "../plugins/backend/actions/put";
+import { PatchElements } from "../plugins/backend/watchers/patchElements";
+import { PatchSignals } from "../plugins/backend/watchers/patchSignals";
+// DataSpa
+import { ViewTransition } from "../plugins/attributes/viewTransitions";
+import { ReplaceUrl } from "../plugins/attributes/replaceUrl";
+import { Persist } from "../plugins/attributes/persist";
+import { ScrollIntoView } from "../plugins/attributes/scrollIntoView";
 
 load(
   // Backend
@@ -48,7 +53,7 @@ load(
   OnIntersect,
   OnInterval,
   OnLoad,
-	OnSignalPatch,
+  OnSignalPatch,
   Ref,
   Show,
   Signals,
@@ -58,8 +63,13 @@ load(
   Peek,
   SetAll,
   ToggleAll,
-)
+  // DataSpa
+  ViewTransition,
+  ReplaceUrl,
+  Persist,
+  ScrollIntoView,
+);
 
-apply()
+apply();
 
-export { apply, load, setAlias }
+export { apply, load, setAlias };
--- library/src/plugins/index.ts
+++ library/src/plugins/index.ts
@@ -1,30 +1,35 @@
-import { Peek } from '../plugins/actions/peek'
-import { SetAll } from '../plugins/actions/setAll'
-import { ToggleAll } from '../plugins/actions/toggleAll'
-import { Attr } from '../plugins/attributes/attr'
-import { Bind } from '../plugins/attributes/bind'
-import { Class } from '../plugins/attributes/class'
-import { Computed } from '../plugins/attributes/computed'
-import { Effect } from '../plugins/attributes/effect'
-import { Indicator } from '../plugins/attributes/indicator'
-import { JsonSignals } from '../plugins/attributes/jsonSignals'
-import { On } from '../plugins/attributes/on'
-import { OnIntersect } from '../plugins/attributes/onIntersect'
-import { OnInterval } from '../plugins/attributes/onInterval'
-import { OnLoad } from '../plugins/attributes/onLoad'
+import { Peek } from "../plugins/actions/peek";
+import { SetAll } from "../plugins/actions/setAll";
+import { ToggleAll } from "../plugins/actions/toggleAll";
+import { Attr } from "../plugins/attributes/attr";
+import { Bind } from "../plugins/attributes/bind";
+import { Class } from "../plugins/attributes/class";
+import { Computed } from "../plugins/attributes/computed";
+import { Effect } from "../plugins/attributes/effect";
+import { Indicator } from "../plugins/attributes/indicator";
+import { JsonSignals } from "../plugins/attributes/jsonSignals";
+import { On } from "../plugins/attributes/on";
+import { OnIntersect } from "../plugins/attributes/onIntersect";
+import { OnInterval } from "../plugins/attributes/onInterval";
+import { OnLoad } from "../plugins/attributes/onLoad";
 import { OnSignalPatch } from "../plugins/attributes/onSignalPatch";
-import { Ref } from '../plugins/attributes/ref'
-import { Show } from '../plugins/attributes/show'
-import { Signals } from '../plugins/attributes/signals'
-import { Style } from '../plugins/attributes/style'
-import { Text } from '../plugins/attributes/text'
-import { DELETE } from '../plugins/backend/actions/delete'
-import { GET } from '../plugins/backend/actions/get'
-import { PATCH } from '../plugins/backend/actions/patch'
-import { POST } from '../plugins/backend/actions/post'
-import { PUT } from '../plugins/backend/actions/put'
-import { PatchElements } from '../plugins/backend/watchers/patchElements'
-import { PatchSignals } from '../plugins/backend/watchers/patchSignals'
+import { Ref } from "../plugins/attributes/ref";
+import { Show } from "../plugins/attributes/show";
+import { Signals } from "../plugins/attributes/signals";
+import { Style } from "../plugins/attributes/style";
+import { Text } from "../plugins/attributes/text";
+import { DELETE } from "../plugins/backend/actions/delete";
+import { GET } from "../plugins/backend/actions/get";
+import { PATCH } from "../plugins/backend/actions/patch";
+import { POST } from "../plugins/backend/actions/post";
+import { PUT } from "../plugins/backend/actions/put";
+import { PatchElements } from "../plugins/backend/watchers/patchElements";
+import { PatchSignals } from "../plugins/backend/watchers/patchSignals";
+// DataSpa additions
+import { Persist } from "../plugins/attributes/persist";
+import { ReplaceUrl } from "../plugins/attributes/replaceUrl";
+import { ScrollIntoView } from "../plugins/attributes/scrollIntoView";
+import { ViewTransition } from "./attributes/viewTransitions";
 
 export {
   // Backend
@@ -47,7 +52,7 @@ export {
   OnIntersect,
   OnInterval,
   OnLoad,
-	OnSignalPatch,
+  OnSignalPatch,
   Ref,
   Show,
   Signals,
@@ -57,4 +62,9 @@ export {
   Peek,
   SetAll,
   ToggleAll,
-}
+  //DataSpa
+  Persist,
+  ReplaceUrl,
+  ScrollIntoView,
+  ViewTransition,
+};
